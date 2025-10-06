1ce2d71b8e77e47a07ac10ecb8a50d9fa69795ea
atho-gitrepo <aungthuheinoo1992@gmail.com>
2025-10-06 20:22:56 +0630

    Update __init__.py

--- worker/esd/sofascore/__init__.py
+++ worker/esd/sofascore/__init__.py
@@ -2,7 +2,14 @@
 Sofascore client module.
 """
 
-from .client import SofascoreClient
+# Import the main client class
+from .client import SofascoreClient 
+
+# Import and expose the specific type needed from the nested module
+from .types.Entity import EntityType 
+
+# Import the entire types submodule as well (for users who want to access other types)
 from . import types
 
-__all__ = ["SofascoreClient", "types"]
+# Update __all__ to include the new symbols you want to expose at the package level
+__all__ = ["SofascoreClient", "EntityType", "types"] 
