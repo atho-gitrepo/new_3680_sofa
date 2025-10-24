1c9ff4cbec2253a16a9ec9db9ae2088c5d01cbf8
atho-gitrepo <aungthuheinoo1992@gmail.com>
2025-10-25 01:01:43 +0630

    Update __init__.py

--- worker/esd/sofascore/__init__.py
+++ worker/esd/sofascore/__init__.py
@@ -1,46 +1,78 @@
-# esd/sofascore/__init__.py
-
+This is init.py.
 """
-Sofascore module
+Contains the types for the Sofascore service.
 """
 
-# Import the main client class
-from .client import SofascoreClient 
+from .event import Event, parse_events, parse_event
+from .status import StatusType, Status
+from .team import Team, parse_team
+from .player import Player, parse_player
+from .player_attributes import PlayerAttributes, parse_player_attributes
+from .transfer import TransferHistory, parse_transfer_history
+from .match_stats import MatchStats, parse_match_stats
+from .lineup import Lineups, PlayerLineup, TeamColor, TeamLineup, parse_lineups
+from .shot import Shot, parse_shots
+from .tournament import Tournament, parse_tournaments, parse_tournament
+from .season import Season, parse_seasons, parse_season
+from .bracket import Bracket, parse_bracket, parse_brackets
+from .standing import Standing, parse_standing, parse_standings
+from .incident import Incident, IncidentType, parse_incident, parse_incidents
+from .top import TopPlayersMatch, parse_top_players_match
+from .comment import Comment, CommentType, parse_comments
+from .top_tournament_teams import TopTournamentTeams, parse_top_tournament_teams
+from .top_tournament_players import TopTournamentPlayers, parse_top_tournament_players
+from .entity import EntityType
+from .categories import Category
 
-# Import and expose all necessary types from the .types submodule
-from .types import (
-    Event,
-    Team,
-    Player,
-    TransferHistory,
-    PlayerAttributes,
-    MatchStats,
-    Lineups,
-    Incident,
-    TopPlayersMatch,
-    Comment,
-    Shot,
-    Tournament,
-    Season,
-    Bracket,
-    Standing,
-    TopTournamentTeams,
-    TopTournamentPlayers,
-    EntityType,
-    Category,
-    # 🟢 CRITICAL FIX: Explicitly import the new class and parser from .types
-    TeamTournamentStats, 
-    parse_team_tournament_stats,
-)
 
-# Import the entire types submodule as well (for users who want to access other types)
-from . import types
-
-# Update __all__ to include the new symbols you want to expose at the package level
 __all__ = [
-    "SofascoreClient", 
-    "EntityType", 
-    "TeamTournamentStats", # Must be listed here
-    "parse_team_tournament_stats", # Must be listed here
-    "types"
+    "Event",
+    "parse_events",
+    "parse_event",
+    "Tournament",
+    "parse_tournaments",
+    "parse_tournament",
+    "TopTournamentPlayers",
+    "parse_top_tournament_players",
+    "TopTournamentTeams",
+    "parse_top_tournament_teams",
+    "Shot",
+    "parse_shots",
+    "Comment",
+    "CommentType",
+    "parse_comments",
+    "TopPlayersMatch",
+    "parse_top_players_match",
+    "Incident",
+    "IncidentType",
+    "parse_incident",
+    "parse_incidents",
+    "Standing",
+    "parse_standing",
+    "parse_standings",
+    "Season",
+    "parse_seasons",
+    "parse_season",
+    "Bracket",
+    "parse_bracket",
+    "parse_brackets",
+    "Team",
+    "parse_team",
+    "Player",
+    "parse_player",
+    "TransferHistory",
+    "parse_transfer_history",
+    "PlayerAttributes",
+    "parse_player_attributes",
+    "MatchStats",
+    "parse_match_stats",
+    "Lineups",
+    "PlayerLineup",
+    "TeamColor",
+    "TeamLineup",
+    "parse_lineups",
+    "EntityType",
+    "Category",
+    "StatusType",
+    "Status",
 ]
